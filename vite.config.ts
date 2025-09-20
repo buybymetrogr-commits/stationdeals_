37d67ea9a64ad4e58adf95321bc849e5d0a07b51
buybymetrogr-commits <buybymetro.gr@gmail.com>
2025-09-20 04:42:29 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,19 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  },
   build: {
     outDir: 'dist',
-    sourcemap: false,
+    sourcemap: true,
+    target: 'es2020',
+    minify: 'esbuild',
     rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'THIS_IS_UNDEFINED') return;
+        warn(warning);
+      },
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
