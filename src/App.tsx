7db03f87a177e3adfbe13e09724674b7c28f06d1
buybymetrogr-commits <buybymetro.gr@gmail.com>
2025-09-20 03:00:39 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { MapPin, Map as MapIcon, X, Train } from 'lucide-react';
 import { Business, BusinessCategory, FilterState, MetroStation } from './types';
 import { categories } from './data/categories';
